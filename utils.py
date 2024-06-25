97114aa3353cbb93d9c5f90a45443cb33b745316
ayanavasarkar <f2013206@dubai.bits-pilani.ac.in>
2024-06-24 21:50:53 -0700

    Add comments to utils

--- utils.py
+++ utils.py
@@ -13,11 +13,27 @@ from sentence_transformers import SentenceTransformer, util
 
 
 class Utils():
+    """
+    A utility class providing various functions for handling language models, 
+    text processing, and database operations.
+    """
 
     def __init__(self) -> None:
+        """
+        Initializes the Utils class with an embedding model from HuggingFace.
+        """
         self.embedding = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
 
-    def load_model(self, api_key):
+    def load_model(self, api_key: str) -> ChatGroq:
+        """
+        Loads the language model using the provided API key.
+
+        Args:
+            api_key (str): The API key for authentication.
+
+        Returns:
+            ChatGroq: An instance of the ChatGroq model.
+        """
         # os.environ.get("GROQ_API_KEY")
         model = ChatGroq(
                 api_key= api_key,
@@ -25,36 +41,74 @@ class Utils():
             )
         return model
 
-    def generic_response(self, model, prompt):
+    def generic_response(self, model: ChatGroq, prompt: str) -> str:
+        """
+        Generates a response to the given prompt using the specified model (LLM)
+
+        Args:
+            model: The language model to be used for generating the response.
+            prompt (str): The input prompt for the model.
+
+        Returns:
+            str: The generated response content.
+        """
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
 
+        # Prepare the messages for the model
         messages = [("system", "You are a helpful assistant. Respond to the user sentence in English."),
             ("human", prompt),
         ]
+
+        # Invoke the model with the prepared messages
         response = model.invoke(messages)
         return response.content
 
-    def load_db(self, path = "/tmp/db"):
-        
+    def load_db(self, path = "/tmp/db") -> Chroma_AYA:
+        """
+        Loads the vector database from the specified path, or returns a message if the path does not exist.
+
+        Args:
+            path (str): The path to the vector database directory.
+
+        Returns:
+            Chroma_AYA: The loaded vector database or a message indicating absence of memory.
+        """
         if os.path.exists(path):
-            # Loading the preexisting DB
-            vectordb = Chroma_AYA(persist_directory=path, 
-                    embedding_function=self.embedding)
-            
+            # Loading the preexisting database
+            vectordb = Chroma_AYA(
+                persist_directory=path, 
+                embedding_function=self.embedding
+            )
         else:
             return "Nothing in Memory"
         
-        # persiste the db to disk
+        # Persist the database to disk
         vectordb.persist()
         return vectordb
 
-    def text_splitter(self, text):
+    def text_splitter(self, text: list) -> list:
+        """
+        Splits the given text into smaller chunks using a recursive character text splitter.
+
+        Args:
+            text (list): The text to be split.
+
+        Returns:
+            list: A list of split text chunks.
+        """
         #splitting the text into
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         texts = text_splitter.split_documents(text)
         return texts
 
     def compare_new_data_to_db(self, splitted_texts):
+        """
+        # TODO: Add This Function for comparing memories and only adding the necessary part.
+        Compares new data to the existing data in the database using sentence embeddings.
+
+        Args:
+            splitted_texts (list): List of text chunks to be compared.
+        """
         model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
         #Compute embedding for both lists
@@ -66,12 +120,17 @@ class Utils():
         #     page_content = each_split.page_content
         # pass
 
-    def store_in_db(self, data_path):
-        
+    def store_in_db(self, data: str) -> None:
+        """
+        Stores the provided data in the vector database, splitting it into chunks if necessary.
+
+        Args:
+            data (str): The data to be stored.
+        """
         persist_directory = '/tmp/db'
         # loader = TextLoader(data_path)
         # documents = loader.load()
-        splitted_texts = self.text_splitter([Document(page_content=data_path, metadata={})])
+        splitted_texts = self.text_splitter([Document(page_content=data, metadata={})])
 
         # new_texts = self.compare_new_data_to_db(splitted_texts)
 
@@ -89,7 +148,17 @@ class Utils():
         # persiste the db to disk
         vectordb.persist()
 
-    def wrap_text_preserve_newlines(self, text, width=110):
+    def wrap_text_preserve_newlines(self, text: str, width=110) -> str:
+        """
+        Wraps the text while preserving newlines.
+
+        Args:
+            text (str): The text to be wrapped.
+            width (int): The width at which to wrap the text.
+
+        Returns:
+            str: The wrapped text with preserved newlines.
+        """
         # Split the input text into lines based on newline characters
         lines = text.split('\n')
 
@@ -101,5 +170,14 @@ class Utils():
 
         return wrapped_text
 
-    def process_llm_response(self, llm_response):
+    def process_llm_response(self, llm_response: dict) -> str:
+        """
+        Processes the LLM response by wrapping the text while preserving newlines.
+
+        Args:
+            llm_response (dict): The response from the LLM.
+
+        Returns:
+            str: The processed response text.
+        """
         return self.wrap_text_preserve_newlines(llm_response['result'])
